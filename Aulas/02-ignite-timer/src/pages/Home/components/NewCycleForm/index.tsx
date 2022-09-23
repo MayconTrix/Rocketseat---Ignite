1ffde348f6c4f78387c6f48a2010cc03f2c4f9bf
MayconTrix <maycon.tiutrix@gmail.com>
2022-09-23 18:15:30 -0300

    P02 Prop Drilling no React

--- Aulas/02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
+++ Aulas/02-ignite-timer/src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,4 +1,25 @@
+import { FormContainer, MinutesAmountContainer, TaskInput } from "./styles";
+import * as zod from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(1, "O cilco precisa ser de no mínimo 5 minutos.")
+    .max(60, "O cilco precisa ser de no máximo 60 minutos."),
+});
+
 export function NewCycleForm() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
   return (
     <FormContainer>
       <label htmlFor="task">Vou trabalhar em</label>
--- Aulas/02-ignite-timer/src/pages/Home/index.tsx
+++ Aulas/02-ignite-timer/src/pages/Home/index.tsx
@@ -6,30 +6,12 @@ import { useEffect, useState } from "react";
 import { differenceInSeconds } from "date-fns";
 
 import {
-  CountdownContainer,
-  FormContainer,
   HomeContainer,
-  MinutesAmountContainer,
-  Separator,
   StarCountdownButton,
   StopCountdownButton,
-  TaskInput,
 } from "./styles";
-import { NewCycleForm } from "./NewCycleForm";
-import { Countdown } from "./Countdown";
-
-const newCycleFormValidationSchema = zod.object({
-  task: zod.string().min(1, "Informe a tarefa"),
-  minutesAmount: zod
-    .number()
-    .min(1, "O cilco precisa ser de no mínimo 5 minutos.")
-    .max(60, "O cilco precisa ser de no máximo 60 minutos."),
-});
-
-// interface NewCycleFormData {
-//   task: string;
-//   minutesAmount: number;
-// }
+import { NewCycleForm } from "./components/NewCycleForm";
+import { Countdown } from "./components/Countdown";
 
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
 
@@ -45,51 +27,8 @@ interface Cycle {
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
-
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
-    resolver: zodResolver(newCycleFormValidationSchema),
-    defaultValues: {
-      task: "",
-      minutesAmount: 0,
-    },
-  });
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
-
-  useEffect(() => {
-    let interval: number;
-
-    if (activeCycle) {
-      interval = setInterval(() => {
-        const secondsDifference = differenceInSeconds(
-          new Date(),
-          activeCycle.startDate
-        );
-
-        if (secondsDifference >= totalSeconds) {
-          setCycles((state) =>
-            state.map((cycle) => {
-              if (cycle.id === activeCycleId) {
-                return { ...cycle, finishedDate: new Date() };
-              } else {
-                return cycle;
-              }
-            })
-          );
-
-          setAmountSecondsPassed(totalSeconds);
-          clearInterval(interval);
-        } else {
-          setAmountSecondsPassed(secondsDifference);
-        }
-      }, 1000);
-    }
-    return () => {
-      clearInterval(interval);
-    };
-  }, [activeCycle, totalSeconds, activeCycleId]);
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
@@ -142,7 +81,11 @@ export function Home() {
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <NewCycleForm />
-        <Countdown />
+        <Countdown
+          activeCycle={activeCycle}
+          setCycles={setCycles}
+          activeCycleId={activeCycleId}
+        />
 
         {activeCycle ? (
           <StopCountdownButton onClick={handleInterruptCycle} type="button">
