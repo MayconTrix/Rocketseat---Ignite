e005e7fe369a17310fdf1735ff2c84dcae9807ab
MayconTrix <maycon.tiutrix@gmail.com>
2022-10-14 17:26:10 -0300

    Add AddressForm/favIcon

--- Desafios/Desafio02-CoffeeDelivery/src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
+++ Desafios/Desafio02-CoffeeDelivery/src/pages/CompleteOrder/components/CompleteOrderForm/AddressForm.tsx
@@ -1,6 +1,7 @@
+import React from "react";
 import { Input } from "../../../../components/Input";
 import { AddressFormContainer } from "./styles";
-import { useFormContext } from "react-hook-form";
+import { useForm, useFormContext } from "react-hook-form";
 
 interface ErrorsType {
   errors: {
@@ -11,21 +12,50 @@ interface ErrorsType {
 }
 
 export function AddressForm() {
-  const { register, formState } = useFormContext();
+  const { register, formState, setValue, setFocus } = useFormContext();
 
   const { errors } = formState as unknown as ErrorsType;
+
+  // const cepCorreios = async (cep: string) => {
+  //   try {
+  //     const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+  //     const data = await response.json();
+  //     return data;
+  //     console.log(data);
+  //     console.log(cepCorreios);
+  //   } catch (error: any) {
+  //     const messageError = error?.response?.data?.errors?.[0]?.message;
+  //     console.log(messageError);
+  //     return false;
+  //   }
+  // };
+
+  const checkCEP = (e: any) => {
+    const cep = e.target.value.replace(/\D/g, "");
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then((res) => res.json())
+      .then((data) => {
+        setValue("street", data.logradouro);
+        setValue("district", data.bairro);
+        setValue("city", data.localidade);
+        setValue("uf", data.uf);
+        setFocus("number");
+      });
+  };
 
   return (
     <AddressFormContainer>
       <Input
         placeholder="CEP"
-        type="number"
+        type="text"
         className="cep"
         {...register("cep")}
         error={errors.cep?.message}
+        onBlur={checkCEP}
       />
       <Input
         placeholder="Rua"
+        type="text"
         className="street"
         {...register("street")}
         error={errors.street?.message}
