7a922c42cdac266b4ee558344f9f9f2b8f4395e3
MayconTrix <maycon.tiutrix@gmail.com>
2022-10-24 13:48:59 -0300

    Add Modal

--- Aulas/03-dt-money/src/components/Header/index.tsx
+++ Aulas/03-dt-money/src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 
 import LogoImg from "../../assets/Logo.svg";
+import * as Dialog from "@radix-ui/react-dialog";
 
 export function Header() {
   return (
@@ -8,7 +9,21 @@ export function Header() {
       <HeaderContent>
         <img src={LogoImg} alt="" />
 
-        <NewTransactionButton>Nova Transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova Transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+
+            <Dialog.Content>
+              <Dialog.Title>Nova Transação</Dialog.Title>
+
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
