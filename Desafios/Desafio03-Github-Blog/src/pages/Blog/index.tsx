41fc5d030b5c718f7273d44c316c13e082747604
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-02 16:01:36 -0300

    Add lib

--- Desafios/Desafio03-Github-Blog/src/pages/Blog/index.tsx
+++ Desafios/Desafio03-Github-Blog/src/pages/Blog/index.tsx
@@ -1,9 +1,47 @@
+import { useCallback, useEffect, useState } from "react";
+import { api } from "../../lib/axios";
 import { Post } from "./components/Post";
 import { Profile } from "./components/Profile";
 import { SearchInput } from "./components/SearchInput";
 import { PostsListContainer } from "./styles";
 
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+const repoName = import.meta.env.VITE_GITHUB_REPONAME;
+
+export interface IPost {
+  title: string;
+  body: string;
+  createdAt: string;
+  number: number;
+  html_url: string;
+  comments: number;
+  user: {
+    login: string;
+  };
+}
 export function Blog() {
+  const [posts, setPosts] = useState<IPost[]>([]);
+
+  const getPosts = useCallback(
+    async (query: string = "") => {
+      try {
+        const response = await api.get(
+          `/search/issues?q=${query}%20repo:${username}/${repoName}`
+        );
+
+        console.log(response.data);
+
+        setPosts(response.data.items);
+      } finally {
+      }
+    },
+    [posts]
+  );
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <>
       <Profile />
