b47c5d0c2a99700545405bbe78d3686554a2ae2b
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-04 14:00:37 -0300

    Add PostContent

--- Desafios/Desafio03-Github-Blog/src/pages/Post/components/PostHeader/index.tsx
+++ Desafios/Desafio03-Github-Blog/src/pages/Post/components/PostHeader/index.tsx
@@ -7,42 +7,63 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useNavigate } from "react-router-dom";
 import { ExternalLink } from "../../../../components/ExterlLink";
+import { Spinner } from "../../../../components/Spinner";
+import { relativeDateFormatter } from "../../../../utils/formatter";
+import { IPost } from "../../../Blog";
 import { PostHeaderContainer } from "./styles";
 
-export function PostHeader() {
+interface PostHeaderProps {
+  postData: IPost;
+  isLoading: boolean;
+}
+
+export function PostHeader({ postData, isLoading }: PostHeaderProps) {
   const navigate = useNavigate();
 
   function goBack() {
     navigate(-1);
   }
 
+  const formattedDate = relativeDateFormatter(postData?.created_at);
+
   return (
     <PostHeaderContainer>
-      <header>
-        <ExternalLink
-          as="button"
-          onClick={goBack}
-          icon={<FontAwesomeIcon icon={faChevronLeft} />}
-          text="Voltar"
-          variant="iconLeft"
-        />
-        <ExternalLink text="Ver no Github" href="#" target="_blank" />
-      </header>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <header>
+            <ExternalLink
+              as="button"
+              onClick={goBack}
+              icon={<FontAwesomeIcon icon={faChevronLeft} />}
+              text="Voltar"
+              variant="iconLeft"
+            />
+            <ExternalLink
+              text="Ver no Github"
+              href={postData.html_url}
+              target="_blank"
+            />
+          </header>
 
-      <h1>JavaScript data types and data structures</h1>
-      <ul>
-        <li>
-          <FontAwesomeIcon icon={faGithub} />
-          MayconTrix
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faCalendar} />
-          Há 1 dia
-        </li>
-        <li>
-          <FontAwesomeIcon icon={faComment} />5 comentários
-        </li>
-      </ul>
+          <h1>{postData.title}</h1>
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faGithub} />
+              {postData.user.login}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faCalendar} />
+              {formattedDate}
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faComment} />
+              {postData.comments} comentários
+            </li>
+          </ul>
+        </>
+      )}
     </PostHeaderContainer>
   );
 }
