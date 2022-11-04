fd836c5a731d79a61c13150895efd6174bb4617c
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-04 10:35:26 -0300

    Add Update Profile

--- Desafios/Desafio03-Github-Blog/src/pages/Blog/components/Profile/index.tsx
+++ Desafios/Desafio03-Github-Blog/src/pages/Blog/components/Profile/index.tsx
@@ -1,36 +1,71 @@
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { faBuilding, faUserGroup } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useCallback, useEffect, useState } from "react";
 import { ExternalLink } from "../../../../components/ExterlLink";
+import { api } from "../../../../lib/axios";
 import { ProfileContainer, ProfileDetails, ProfilePicture } from "./styles";
 
+const username = import.meta.env.VITE_GITHUB_USERNAME;
+
+interface ProfileData {
+  login: string;
+  bio: string;
+  avatar_url: string;
+  html_url: string;
+  name: string;
+  company: string;
+  followers: number;
+}
+
 export function Profile() {
+  const [profileData, setProfileData] = useState<ProfileData>(
+    {} as ProfileData
+  );
+  const [isLoading, setIsLoading] = useState(true);
+  const getProfileData = useCallback(async () => {
+    try {
+      setIsLoading(true);
+      const response = await api.get(`/users/${username}`);
+
+      setProfileData(response.data);
+    } finally {
+      setIsLoading(false);
+    }
+  }, [profileData]);
+
+  useEffect(() => {
+    getProfileData();
+  }, []);
+
   return (
     <ProfileContainer>
-      <ProfilePicture src="https://github.com/MayconTrix.png" />
+      <ProfilePicture src={profileData.avatar_url} />
 
       <ProfileDetails>
         <header>
-          <h1>Maycon Batista</h1>
-          <ExternalLink text="GitHub" href="#" />
+          <h1>{profileData.name}</h1>
+          <ExternalLink
+            text="GitHub"
+            href={profileData.html_url}
+            target="_blank"
+          />
         </header>
-        <p>
-          Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
-          viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat
-          pulvinar vel mass.
-        </p>
+        <p>{profileData.bio}</p>
         <ul>
           <li>
             <FontAwesomeIcon icon={faGithub} />
-            MayconTrix
+            {profileData.login}
           </li>
-          <li>
-            <FontAwesomeIcon icon={faBuilding} />
-            RembrandTech
-          </li>
+          {profileData?.company && (
+            <li>
+              <FontAwesomeIcon icon={faBuilding} />
+              {profileData.company}
+            </li>
+          )}
           <li>
             <FontAwesomeIcon icon={faUserGroup} />
-            234567 seguidores
+            {profileData.followers} seguidores
           </li>
         </ul>
       </ProfileDetails>
