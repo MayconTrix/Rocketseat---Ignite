c095feba4b5a7dcf4ef134c670d2b987a2d2553d
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-09 13:23:41 -0300

    Add new styles

--- Aulas/04-ignite-shop/src/pages/product/[id].tsx
+++ Aulas/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,7 +1,30 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
 
-  return <h1>Product {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Asperiores
+          excepturi, blanditiis quod quo ex aut impedit atque aspernatur,
+          tempora repellendus quaerat! Quidem recusandae sapiente
+          exercitationem, ab nisi at illo praesentium?
+        </p>
+
+        <button>Comprar agora</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
