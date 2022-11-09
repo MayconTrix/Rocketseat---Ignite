ec287572a54f28d55d4d44d9eb8f1871add331ba
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-09 18:36:29 -0300

    Add checkout stripe

--- Aulas/04-ignite-shop/src/pages/product/[id].tsx
+++ Aulas/04-ignite-shop/src/pages/product/[id].tsx
@@ -15,10 +15,14 @@ interface ProductProps {
     imageUrl: string;
     price: string;
     description: string;
+    defaultPriceId: string;
   };
 }
 
 export default function Product({ product }: ProductProps) {
+  function handleBuyProduct() {
+    console.log(product.defaultPriceId);
+  }
   return (
     <ProductContainer>
       <ImageContainer>
@@ -29,7 +33,7 @@ export default function Product({ product }: ProductProps) {
         <span>{product.price}</span>
         <p>{product.description}</p>
 
-        <button>Comprar agora</button>
+        <button onClick={handleBuyProduct}>Comprar agora</button>
       </ProductDetails>
     </ProductContainer>
   );
@@ -64,6 +68,7 @@ export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
           currency: "BRL",
         }).format(price.unit_amount / 100),
         description: product.description,
+        defaultPriceId: price.id,
       },
     },
     revalidate: 60 * 60 * 1, // 1 hour
