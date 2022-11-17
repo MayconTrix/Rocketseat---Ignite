2076b54db8aec330992d2cd2764274135ca2b0d3
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-17 18:41:58 -0300

    Page Signin

--- Aulas/04-dashgo/src/pages/index.tsx
+++ Aulas/04-dashgo/src/pages/index.tsx
@@ -1,3 +1,68 @@
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack,
+} from "@chakra-ui/react";
+
 export default function Home() {
-  return <h1>Dashgo</h1>;
+  return (
+    <Flex
+      width="100vw"
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth="360px"
+        background="gray.800"
+        padding="8"
+        borderRadius="8"
+        flexDirection="column"
+      >
+        <Stack>
+          <FormControl>
+            <FormLabel htmlFor="email">E-mail</FormLabel>
+
+            <Input
+              id="email"
+              name="email"
+              type="email"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                backgroundColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel htmlFor="password">Password</FormLabel>
+
+            <Input
+              id="password"
+              name="password"
+              type="password"
+              focusBorderColor="pink.500"
+              backgroundColor="gray.900"
+              variant="filled"
+              _hover={{
+                backgroundColor: "gray.900",
+              }}
+              size="lg"
+            />
+          </FormControl>
+        </Stack>
+
+        <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
