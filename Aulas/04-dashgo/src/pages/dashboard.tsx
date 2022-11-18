1ad1a0c35142098183c43ec77ba8e5959d9eda3a
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-18 15:00:16 -0300

    Add Component Sidebar

--- Aulas/04-dashgo/src/pages/dashboard.tsx
+++ Aulas/04-dashgo/src/pages/dashboard.tsx
@@ -1,10 +1,21 @@
 import { Flex } from "@chakra-ui/react";
 import { Header } from "../components/Header";
+import { SideBar } from "../components/Sidebar";
 
 export default function Dashboard() {
   return (
-    <Flex>
+    <Flex direction="column" height="100vh">
       <Header />
+
+      <Flex
+        width="100%"
+        marginY="6"
+        marginX="auto"
+        paddingX="6"
+        maxWidth={1480}
+      >
+        <SideBar />
+      </Flex>
     </Flex>
   );
 }
