9fcead92055ca8d537dbb653c73c76207d566fd9
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-21 17:26:13 -0300

    Header responsive

--- Aulas/04-dashgo/src/components/Header/Profile.tsx
+++ Aulas/04-dashgo/src/components/Header/Profile.tsx
@@ -1,16 +1,26 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box marginRight="4" textAlign="right">
-        <Text>Maycon Batista</Text>
-        <Text color="gray.300" fontSize="small">
-          maycon.tiutrix@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box marginRight="4" textAlign="right">
+          <Text>Maycon Batista</Text>
+          <Text color="gray.300" fontSize="small">
+            maycon.tiutrix@gmail.com
+          </Text>
+        </Box>
+      )}
 
-      <Avatar size="md" name="Maycon Batista" src="" />
+      <Avatar
+        size="md"
+        name="Maycon Batista"
+        src="https://github.com/MayconTrix.png"
+      />
     </Flex>
   );
 }
