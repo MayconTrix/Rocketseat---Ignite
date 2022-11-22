4bb5e7183553b21044ad4df82ef74dcc21f8efbc
MayconTrix <maycon.tiutrix@gmail.com>
2022-11-22 14:06:06 -0300

    Autentication form

--- Aulas/04-dashgo/src/pages/index.tsx
+++ Aulas/04-dashgo/src/pages/index.tsx
@@ -1,7 +1,19 @@
-import { Button, Flex, FormControl, FormLabel, Stack } from "@chakra-ui/react";
+import { Button, Flex, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 import { Input } from "../components/Form";
 
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
 export default function SignIn() {
+  const { register, handleSubmit } = useForm();
+
+  const handleSignIn: SubmitHandler<SignInFormData> = (values) => {
+    console.log(values);
+  };
+
   return (
     <Flex
       width="100vw"
@@ -17,10 +29,21 @@ export default function SignIn() {
         padding="8"
         borderRadius="8"
         flexDirection="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack>
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="password" type="password" label="Senha" />
+          <Input
+            name="email"
+            type="email"
+            label="E-mail"
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            type="password"
+            label="Senha"
+            {...register("password")}
+          />
         </Stack>
 
         <Button type="submit" marginTop="6" colorScheme="pink" size="lg">
